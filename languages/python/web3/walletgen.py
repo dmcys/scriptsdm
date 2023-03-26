536ce9fb4a747a7b48060d9d5949c167dc3fc340
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-26 05:55:15 +0000

    Update walletgen.py

--- languages/python/web3/walletgen.py
+++ languages/python/web3/walletgen.py
@@ -2,25 +2,6 @@ import secrets
 from eth_keys import keys
 import os
 
-# Prompt the user to select encryption method
-encrypt = input("Do you want to encrypt wallets.txt using GPG? (y/n) ")
-if encrypt.lower() == "y":
-    # Prompt the user to select a key or generate a new one
-    new_key = input("Do you want to generate a new key? (y/n) ")
-    if new_key.lower() == "y":
-        # Generate a new key and encrypt wallets.txt
-        encrypt_times = int(input("How many times do you want to encrypt wallets.txt? "))
-        os.system(f"gpg --gen-key && gpg -c --cipher-algo AES256 --s2k-digest-algo SHA512 wallets.txt")
-        for i in range(1, encrypt_times):
-            os.system(f"gpg -c --cipher-algo AES256 --s2k-digest-algo SHA512 wallets.txt.gpg")
-    else:
-        # Use an existing key and encrypt wallets.txt
-        key_path = input("Enter the path to your GPG key: ")
-        encrypt_times = int(input("How many times do you want to encrypt wallets.txt? "))
-        os.system(f"gpg -e --cipher-algo AES256 --s2k-digest-algo SHA512 --recipient-file {key_path} wallets.txt")
-        for i in range(1, encrypt_times):
-            os.system(f"gpg -e --cipher-algo AES256 --s2k-digest-algo SHA512 --recipient-file {key_path} wallets.txt.gpg")
-
 # Prompt the user to select how many wallets to generate
 num_wallets = int(input("How many wallets do you want to generate? "))
 
@@ -42,3 +23,26 @@ with open("wallets.txt", "w") as file:
         file.write(f"{private_key}\n")
         file.write("Public key:\n")
         file.write(f"{public_key}\n\n")
+
+# Prompt the user to select encryption method
+encrypt = input("Do you want to encrypt wallets.txt using GPG? (y/n) ")
+if encrypt.lower() == "y":
+    # Prompt the user to select a key or generate a new one
+    new_key = input("Do you want to generate a new key? (y/n) ")
+    if new_key.lower() == "y":
+        # Generate a new key and encrypt wallets.txt
+        encrypt_times = int(input("How many times do you want to encrypt wallets.txt? "))
+        os.system(f"gpg --gen-key && gpg -c --cipher-algo AES256 --s2k-digest-algo SHA512 wallets.txt")
+        for i in range(1, encrypt_times):
+            os.system(f"gpg -c --cipher-algo AES256 --s2k-digest-algo SHA512 wallets.txt.gpg")
+            if os.path.exists("wallet.txt"):
+                    os.remove("wallet.txt")
+    else:
+        # Use an existing key and encrypt wallets.txt
+        key_path = input("Enter the path to your GPG key: ")
+        encrypt_times = int(input("How many times do you want to encrypt wallets.txt? "))
+        os.system(f"gpg -e --cipher-algo AES256 --s2k-digest-algo SHA512 --recipient-file {key_path} wallets.txt")
+        for i in range(1, encrypt_times):
+            os.system(f"gpg -e --cipher-algo AES256 --s2k-digest-algo SHA512 --recipient-file {key_path} wallets.txt.gpg")
+         if os.path.exists("wallet.txt"):
+                 os.remove("wallet.txt")
