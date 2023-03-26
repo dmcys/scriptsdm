347a405f0077d304160ec244d94674e67f4f3966
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-26 07:22:22 +0000

    Create walletgen_.py

---
+++ languages/python/web3/walletgen_.py
@@ -0,0 +1,23 @@
+import secrets
+from eth_keys import keys
+num_wallets = int(input("How many wallets do you want to generate? "))
+
+with open("wallets.txt", "w") as file:
+    for i in range(1, num_wallets+1):
+# Generate a 32-byte random string
+        private_key_bytes = secrets.token_bytes(32)
+
+# Convert the bytes to a private key
+        private_key = keys.PrivateKey(private_key_bytes)
+
+# Get the public key from the private key
+        public_key = private_key.public_key
+
+# Print both keys to the console
+#        print("Private key:", private_key)
+#        print("Public key:", public_key)
+        file.write(f"Wallet {i}\n")
+        file.write("Private key:\n")
+        file.write(f"{private_key}\n")
+        file.write("Public key:\n")
+        file.write(f"{public_key}\n\n")
