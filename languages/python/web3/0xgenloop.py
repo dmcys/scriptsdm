0b968197f1c8e6abcb83efcba23ad10048fd7894
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-26 06:53:23 +0000

    Update 0xgenloop.py

--- languages/python/web3/0xgenloop.py
+++ languages/python/web3/0xgenloop.py
@@ -1,33 +1,23 @@
-import secrets
-from eth_keys import keys
-# Generate a 32-byte random string
-private_key_bytes = secrets.token_bytes(32)
-# Convert the bytes to a private key
-private_key = keys.PrivateKey(private_key_bytes)
-# Get the public key from the private key
-public_key = private_key.public_key
-# Write the wallet info to wallets.txt
-file.write(f"Wallet {i}\n")
-file.write("Private key:\n")
-file.write(f"{private_key}\n")
-file.write("Public key:\n")
-file.write(f"{public_key}\n\n")
-while True:
-# Open the wallets.txt file in append mode
-with open("wallets.txt", "a") as file:
-# Generate a 32-byte random string
-private_key_bytes = secrets.token_bytes(32)
-# Convert the bytes to a private key
-private_key = keys.PrivateKey(private_key_bytes)
-# Get the public key from the private key
-public_key = private_key.public_key
-# Write the wallet info to wallets.txt
-file.write(f"Wallet {i}\n")
-file.write("Private key:\n")
-file.write(f"{private_key}\n")
-file.write("Public key:\n")
-file.write(f"{public_key}\n\n")
-# Prompt the user to continue or quit
-choice = input("Do you want to generate more wallets? (Y/N) ")
-if choice.upper() == "N":
-   break
+from web3.auto import w3
+from eth_account import Account
+import msvcrt
+
+num_wallets = int(input("How many wallets do you want to generate? "))
+
+with open("wallets.txt", "w") as file:
+    for i in range(1, num_wallets+1):
+        new_account = Account.create()
+        private_key = new_account.privateKey.hex()
+        public_key = new_account.publicKey.hex()
+        file.write(f"Wallet {i}\n")
+        file.write("Private key:\n")
+        file.write(f"{private_key}\n")
+        file.write("Public key:\n")
+        file.write(f"{public_key}\n\n")
+        
+        if msvcrt.kbhit():
+            break
+            
+        print("Press any key to stop...")
+        
+    print("Done!")
