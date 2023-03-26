1e291a87798ea6746f132102b1ebf2501bc18930
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-26 20:22:15 +0000

    fix issue of /usr/bin shell noting detected
    opening a new shell a new terminal and closing the old to update /usr/bin with a new symbolic links

--- softwares/websites/cdnpages/surge/portable_nodejs.sh
+++ softwares/websites/cdnpages/surge/portable_nodejs.sh
@@ -13,14 +13,32 @@ curl -sL "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-x
 # Set the PATH environment variable to use the locally installed Node.js
 export PATH="$(pwd)/nodejs/bin:$PATH"
 
-# Install npm in the project directory
-npm config set prefix "$(pwd)/node_modules"
-npm install -g npm@latest
-
 # Create a symbolic link for npm and npx in the project directory
 sudo ln -s "$(pwd)/node_modules/bin/npm" /usr/local/bin/npm
 sudo ln -s "$(pwd)/node_modules/bin/npx" /usr/local/bin/npx
 sudo ln -s "$(pwd)/nodejs/bin/node" /usr/local/bin/node
+
+# Get the name of the user's terminal emulator
+TERMINAL_EMULATOR=$(basename "$SHELL")
+
+# Open a new shell with bash -u and execute the npm installation in the new shell
+case "$TERMINAL_EMULATOR" in
+"gnome-terminal"|"tilix"|"xfce4-terminal"|"konsole"|"terminator"|"mate-terminal")
+"$TERMINAL_EMULATOR" -- bash -c 'npm config set prefix "$(pwd)/node_modules"; npm install -g npm@latest; exec bash'
+;;
+"xterm"|"rxvt"|"urxvt"|"st"|"sakura"|"eterm"|"lilyterm"|"terminology"|"terminix"|"cool-retro-term"|"deepin-terminal"|"tilda")
+"$TERMINAL_EMULATOR" -e bash -c 'npm config set prefix "$(pwd)/node_modules"; npm install -g npm@latest; exec bash'
+;;
+*)
+echo "Unable to open a new terminal. Please open a new terminal manually and run the following command:"
+echo "npm config set prefix "$(pwd)/node_modules"; npm install -g npm@latest"
+;;
+esac
+
+# rerun npm in the project directory
+npm config set prefix "$(pwd)/node_modules"
+npm install -g npm@latest
+
 
 # Print success message
 echo "Node.js and npm installed successfully!"
