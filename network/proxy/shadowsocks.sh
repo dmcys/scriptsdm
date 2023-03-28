4b5ec4d6dfd00c7657804e462e49f454973457c1
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-28 07:21:23 +0000

    Update shadowsocks.sh

--- network/proxy/shadowsocks.sh
+++ network/proxy/shadowsocks.sh
@@ -1,19 +1,4 @@
 #!/bin/bash
-firewall() {
-# porta do Shadowsocks (altere caso necessário)
-local PORTA=1080
-# verifica se o serviço Shadowsocks está em execução
-if systemctl is-active shadowsocks > /dev/null; then
-  echo "Shadowsocks em execução. Liberando porta $PORTA no firewall..."
-# libera a porta no firewall
-  sudo ufw allow $PORTA
- else
-  echo "Shadowsocks não está em execução. Fechando porta $PORTA no firewall..."
-# fecha a porta no firewall
-  sudo ufw delete allow $PORTA
-fi
-}
-
 
 install_shadowsocks() {
 install_shadowscks() {
@@ -83,3 +68,125 @@ else
 fi
 }
 uninstall_shadowsocks
+
+configure_shadowsocks() {
+configure_shadowscks(){
+    echo "Configuring Shadowsocks..."
+    
+    # Default values
+    SERVER="0.0.0.0"
+    SERVER_PORT=8388
+    LOCAL_ADDRESS="127.0.0.1"
+    LOCAL_PORT=1080
+    PASSWORD="your_password_here"
+    TIMEOUT=300
+    METHOD="chacha20-ietf-poly1305"
+
+    # Read input from user
+    read -p "Enter server IP address (default: $SERVER): " input_server
+    read -p "Enter server port (default: $SERVER_PORT): " input_server_port
+    read -p "Enter local address (default: $LOCAL_ADDRESS): " input_local_address
+    read -p "Enter local port (default: $LOCAL_PORT): " input_local_port
+    read -p "Enter password (default: $PASSWORD): " input_password
+    read -p "Enter timeout in seconds (default: $TIMEOUT): " input_timeout
+    read -p "Enter encryption method (default: $METHOD): " input_method
+
+    # Set values to input or default
+    SERVER=${input_server:-$SERVER}
+    SERVER_PORT=${input_server_port:-$SERVER_PORT}
+    LOCAL_ADDRESS=${input_local_address:-$LOCAL_ADDRESS}
+    LOCAL_PORT=${input_local_port:-$LOCAL_PORT}
+    PASSWORD=${input_password:-$PASSWORD}
+    TIMEOUT=${input_timeout:-$TIMEOUT}
+    METHOD=${input_method:-$METHOD}
+
+    # Write configuration file
+    cat > /etc/shadowsocks-libev/config.json <<EOF
+{
+    "server":"$SERVER",
+    "server_port":$SERVER_PORT,
+    "local_address":"$LOCAL_ADDRESS",
+    "local_port":$LOCAL_PORT,
+    "password":"$PASSWORD",
+    "timeout":$TIMEOUT,
+    "method":"$METHOD"
+}
+EOF"
+
+    echo "Shadowsocks configuration file written to /etc/shadowsocks-libev/config.json"
+    }
+# Ask user for input
+read -p "Do you want to install Shadowsocks? (y/n) " choice
+# Check user input
+if [[ "$choice" == [yY] ]]; then
+  configure_shadowscks
+else
+  echo "Shadowsocks configuration canceled by user"
+fi
+}
+configure_shadowsocks
+
+whonix() {
+whnix(){
+#!/bin/bash
+# Install Shadowsocks
+sudo apt-get update
+sudo apt-get install shadowsocks-libev -y
+configure_shadowsocks
+
+# Start Shadowsocks
+sudo systemctl start shadowsocks-libev
+# Allow incoming connections to Shadowsocks port
+sudo ufw allow 8388
+# Allow outgoing connections to Tor ports
+sudo ufw allow out 9150
+sudo ufw allow out 9050
+# Set up Tor as the default gateway
+sudo sed -i '/DisableNetwork/c\DisableNetwork 0' /etc/tor/torrc
+sudo sed -i '/TransPort/c\TransPort 9040' /etc/tor/torrc
+sudo sed -i '/TransListenAddress/c\TransListenAddress 127.0.0.1' /etc/tor/torrc
+sudo sed -i '/DNSPort/c\DNSPort 5353' /etc/tor/torrc
+sudo sed -i '/DNSListenAddress/c\DNSListenAddress 127.0.0.1' /etc/tor/torrc
+sudo systemctl restart tor
+# Allow incoming connections to Tor SocksPort
+sudo ufw allow 9050
+# Enable UFW firewall
+sudo ufw --force enable
+# Print configuration information
+echo "Shadowsocks server running on port 8388"
+echo "Make sure to configure your Shadowsocks client to connect to this server"
+    }
+# Ask user for input
+read -p "Do you want to install Shadowsocks? (y/n) " choice
+# Check user input
+if [[ "$choice" == [yY] ]]; then
+  whnix
+else
+  echo "Whonix configuration canceled by user"
+fi
+}
+
+firewall() {
+firewll() {
+# porta do Shadowsocks (altere caso necessário)
+local PORTA=1080
+# verifica se o serviço Shadowsocks está em execução
+if systemctl is-active shadowsocks > /dev/null; then
+  echo "Shadowsocks em execução. Liberando porta $PORTA no firewall..."
+# libera a porta no firewall
+  sudo ufw allow $PORTA
+ else
+  echo "Shadowsocks não está em execução. Fechando porta $PORTA no firewall..."
+# fecha a porta no firewall
+  sudo ufw delete allow $PORTA
+fi
+    }
+# Ask user for input
+read -p "Do you want to install Shadowsocks? (y/n) " choice
+# Check user input
+if [[ "$choice" == [yY] ]]; then
+  firewll
+else
+  echo "Firewall monitor canceled by user"
+fi
+}
