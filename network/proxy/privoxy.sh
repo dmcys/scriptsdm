1ebb915543c60bf4ff61ca587849463636ca168f
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-03-28 06:48:53 +0000

    Update privoxy.sh

--- network/proxy/privoxy.sh
+++ network/proxy/privoxy.sh
@@ -26,6 +26,7 @@ fi
 install_privoxy
 
 uninstall_privoxy() {
+uninstall_prvoxy() {
 # Stop Privoxy service
 sudo systemctl stop privoxy
 # Remove Privoxy package
@@ -34,5 +35,17 @@ sudo apt remove privoxy -y
 sudo rm -rf /etc/privoxy/
 sudo rm -rf /var/log/privoxy/
 echo "Privoxy configuration has been removed successfully."
+}
+# Ask user for input
+read -p "Do you want to install and configure Privoxy? (y/n) " choice
+# Check user input
+if [[ "$choice" == [yY] ]]; then
+  read -p "Enter the port number to use for Privoxy (default is 8118): " port
+  port=${port:-8118}
+  uninstall_prvoxy
+else
+  echo "Privoxy installation canceled by user"
+fi
+}
 }
 uninstall_privoxy
