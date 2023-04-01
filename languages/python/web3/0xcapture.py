b572eb78eeca1e549e3073c228ffa5b2326edac2
DeFiMinds <108605828+defiminds@users.noreply.github.com>
2023-04-01 22:22:30 +0100

    Update 0xcapture.py

--- languages/python/web3/0xcapture.py
+++ languages/python/web3/0xcapture.py
@@ -1,5 +1,8 @@
 #!/bin/bash
+0xcap() {
 # Scan the wallets.txt file for lines containing "Publickey:0x0"
 # and copy them to a new file named 0xwallets.txt,
 # skipping one line from the previous content
 grep "Publickey:0x0" wallets.txt | awk '{print prev "\n" $0; prev=$0}' > 0xwallets.txt
+}
+0xcap
